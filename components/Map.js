456aa0f2f9b9c486657e5724e9559ae974bb1f3d
Alexander Janssen <ajn.janssen@gmail.com>
2025-02-16 17:43:10 +0100

    updated leaflet

--- components/Map.js
+++ components/Map.js
@@ -15,15 +15,30 @@ const Map = ({ location }) => {
   };
 
   return (
-    <div style={{ height: "100%", width: "100%" }}>
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
+    >
       <MapContainer
         center={[52.3676, 4.9041]}
-        zoom={13}
-        style={{ height: "100%", width: "100%" }}
+        className="rounded-xl"
+        zoom={10}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          className="map-tiles"
+        />
+        <TileLayer
+          url="https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=9a947b2b6130ce57fb318b02748ff301"
+          attribution='&copy; <a href="https://openweathermap.org/">OpenWeatherMap</a>'
+          className="map-tiles"
         />
         {location && <Marker position={[location.lat, location.lon]} />}
         <MapCenter location={location} />
